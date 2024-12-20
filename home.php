daf35c69834651628cee9919095601d72e7dc573
ARYANBHAT-eng <117040620+ARYANBHAT-eng@users.noreply.github.com>
2024-12-20 20:54:07 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="admin/assets/wysiwyg/css/froala_editor.css">
+  <link rel="stylesheet" href="admin/assets/wysiwyg/css/froala_style.css">
+<?php 
+include 'admin/db_connect.php'; 
+?>
+<style>
+    #cat-list li{
+        cursor: pointer;
+    }
+       #cat-list li:hover {
+        color: white;
+        background: #007bff8f;
+    }
+    .prod-item p{
+        margin: unset;
+    }
+    .bid-tag {
+    position: absolute;
+    right: .5em;
+}
+</style>
+<?php 
+$cid = isset($_GET['category_id']) ? $_GET['category_id'] : 0;
+?>
+<div class="container">
+    <div class="col-lg-12">
+      <div class="fr-wrapper">
+      According to a UGC funded study  close to 40% students in colleges across India face some kind of ragging and only 8.6% reported the incidents and rest of the students kept Quiet .
+ The main reason was not having a stable anonymous platform which provided them with a sense of safety while reporting complaints about their situations without any hesitation. So there is a need of a platform where students can speak freely about all the ragging /harassment issues they are facing or have faced up until now.
+      </div>
+    </div>
+</div>
+       
+<script>
+    
+</script>
